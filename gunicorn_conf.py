0f98c94c79700cda2c3372eea159992b9a45fd86
Hiskia Pratama <hiskianggi@gmail.com>
2023-06-14 08:26:39 +0000

    feat: gunicorn conf

---
+++ gunicorn_conf.py
@@ -0,0 +1,13 @@
+# gunicorn_conf.py
+from multiprocessing import cpu_count
+
+bind = "127.0.0.1:8000"
+
+# Worker Options
+workers = cpu_count() + 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'debug'
+accesslog = '/home/m163dsx2931/recommender/access_log'
+errorlog =  '/home/m163dsx2931/recommender/error_log'
